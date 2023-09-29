f48c20ac50a8e7336acff4c40e7c0d7c0349677f
sparkels36 <piyush362002@gmail.com>
2023-09-29 13:18:48 +0530

    forgot password option added 1

--- src/main/java/mini/mini/ForgotPasswordController.java
+++ src/main/java/mini/mini/ForgotPasswordController.java
@@ -3,6 +3,7 @@ package mini.mini;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,15 +11,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ResourceBundle;
 
-public class ForgotPasswordController {
+public class ForgotPasswordController implements Initializable {
 
     @FXML
     private TextField email;
@@ -57,6 +63,10 @@ public class ForgotPasswordController {
     private TextField username;
     @FXML
     private Button subitid;
+
+    @FXML
+    private ImageView forimage;
+
 
     public void onSubmitButtonClick(ActionEvent event){
         System.out.println("Button clicked!");
@@ -176,5 +186,11 @@ public class ForgotPasswordController {
         stage.setTitle("Login");
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File backFile75 = new File("im/password-reset-service-2023-01-04 00-00-00-2023-06-17 02-19-41.png");
+        Image backImage75 = new Image(backFile75.toURI().toString());
+        forimage.setImage(backImage75);
+    }
 }
 
